aaa440196b927e9a35c9064ada9845d83109c2af
technoryta <technoryta@gmail.com>
2024-03-23 07:05:13 -0700

    update code

--- models/base_model.py
+++ models/base_model.py
@@ -3,22 +3,24 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """Base class"""
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
 
     def __init__(self):
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
 
     def __str__(self):
         return (f"[{self.__class__.__name__}]({self.id}){self.__dict__}")
 
     def save(self):
-        self.update_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        return self.__dict__
-        
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
+        classdict = self.__dict__
+        classdict["__class__"] = self.__class__.__name__
+        return (classdict)
